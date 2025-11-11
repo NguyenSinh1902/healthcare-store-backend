4020c956d3532239814f365a889c2e521856766e
NguyenSinh1902 <lenguyensinh1902@gmail.com>
2025-11-11 16:49:42 +0700

    feat(order): add new order mappers & repositories

---
+++ src/main/java/iuh/fit/se/repositories/OrderDetailRepository.java
@@ -0,0 +1,9 @@
+package iuh.fit.se.repositories;
+
+import iuh.fit.se.entities.order.OrderDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
+}
---
+++ src/main/java/iuh/fit/se/repositories/OrderRepository.java
@@ -0,0 +1,12 @@
+package iuh.fit.se.repositories;
+
+import iuh.fit.se.entities.order.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByUser_IdUser(Long userId);
+}

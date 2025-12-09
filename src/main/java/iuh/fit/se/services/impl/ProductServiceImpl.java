ce019b37a0ae633dd6d03e25553d630360bcffd1
MihnThihn <pvmthinh2002@gmail.com>
2025-12-10 01:12:50 +0700

    feat(product): upadte repository(ActiveTrue) & ProductServiceImpl(sort delete and upload file image)

--- src/main/java/iuh/fit/se/services/impl/ProductServiceImpl.java
+++ src/main/java/iuh/fit/se/services/impl/ProductServiceImpl.java
@@ -1,19 +1,24 @@
 package iuh.fit.se.services.impl;
 
-import iuh.fit.se.dtos.product.ProductDetailDTO;
 import iuh.fit.se.dtos.product.ProductRequestDTO;
 import iuh.fit.se.dtos.product.ProductResponseDTO;
-import iuh.fit.se.entities.category.Category;
+import iuh.fit.se.dtos.product.ProductDetailDTO;
 import iuh.fit.se.entities.product.Product;
+import iuh.fit.se.entities.category.Category;
 import iuh.fit.se.entities.product.ProductGroup;
 import iuh.fit.se.exceptions.NotFoundException;
 import iuh.fit.se.mappers.ProductMapper;
 import iuh.fit.se.repositories.CategoryRepository;
 import iuh.fit.se.repositories.ProductRepository;
+import iuh.fit.se.services.CloudinaryService;
 import iuh.fit.se.services.ProductService;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,10 +29,13 @@ public class ProductServiceImpl implements ProductService {
     private final CategoryRepository categoryRepository;
     private final ProductMapper productMapper;
 
-    public ProductServiceImpl(ProductRepository productRepository, CategoryRepository categoryRepository, ProductMapper productMapper) {
+    private final CloudinaryService cloudinaryService;
+
+    public ProductServiceImpl(ProductRepository productRepository, CategoryRepository categoryRepository, ProductMapper productMapper, CloudinaryService cloudinaryService) {
         this.productRepository = productRepository;
         this.categoryRepository = categoryRepository;
         this.productMapper = productMapper;
+        this.cloudinaryService = cloudinaryService;
     }
 
     //Get Products By Group
@@ -41,7 +49,7 @@ public class ProductServiceImpl implements ProductService {
             throw new NotFoundException("Invalid product group: " + productGroup);
         }
 
-        List<Product> products = productRepository.findByProductGroup(groupEnum);
+        List<Product> products = productRepository.findByProductGroupAndActiveTrue(groupEnum);
         if (products.isEmpty()) {
             throw new NotFoundException("No products found for group: " + groupEnum);
         }
@@ -55,7 +63,7 @@ public class ProductServiceImpl implements ProductService {
     //Get products by category
     @Override
     public List<ProductResponseDTO> getProductsByCategory(Long categoryId) {
-        List<Product> products = productRepository.findByCategory_IdCategory(categoryId);
+        List<Product> products = productRepository.findByCategory_IdCategoryAndActiveTrue(categoryId);
         if (products.isEmpty()) {
             throw new NotFoundException("No products found for category ID: " + categoryId);
         }
@@ -75,7 +83,9 @@ public class ProductServiceImpl implements ProductService {
     //Get All
     @Override
     public List<ProductResponseDTO> getAllProducts() {
-        List<Product> products = productRepository.findAll();
+
+        List<Product> products = productRepository.findByActiveTrue();
+
         if (products.isEmpty()) {
             throw new NotFoundException("No products found");
         }
@@ -84,37 +94,115 @@ public class ProductServiceImpl implements ProductService {
                 .collect(Collectors.toList());
     }
 
+    private void calculateAndSetPrice(Product product) {
+        if (product.getOldPrice() != null && product.getDiscountPercent() != null) {
+            double discountFactor = (100.0 - product.getDiscountPercent()) / 100.0;
+            double finalPrice = product.getOldPrice() * discountFactor;
+
+            product.setPrice(finalPrice);
+        }
+
+        else if (product.getPrice() == null && product.getOldPrice() != null) {
+            product.setPrice(product.getOldPrice());
+        }
+    }
+
+    private void processThumbnails(ProductRequestDTO dto, List<MultipartFile> thumbnailFiles) throws IOException {
+        List<String> finalThumbnails = new ArrayList<>();
+
+        if (dto.getThumbnails() != null) {
+            finalThumbnails.addAll(dto.getThumbnails());
+        }
+
+        if (thumbnailFiles != null && !thumbnailFiles.isEmpty()) {
+            for (MultipartFile file : thumbnailFiles) {
+                if (!file.isEmpty()) {
+                    String url = cloudinaryService.uploadImage(file);
+                    finalThumbnails.add(url);
+                }
+            }
+            dto.setThumbnails(finalThumbnails);
+        }
+    }
+
     //Create product
     @Transactional
     @Override
-    public ProductResponseDTO createProduct(ProductRequestDTO dto) {
+    public ProductResponseDTO createProduct(ProductRequestDTO dto, MultipartFile imageFile, List<MultipartFile> thumbnailFiles) throws IOException {
         Category category = categoryRepository.findById(dto.getIdCategory())
                 .orElseThrow(() -> new NotFoundException("Category not found with ID: " + dto.getIdCategory()));
 
+        if (imageFile != null && !imageFile.isEmpty()) {
+
+            String imageUrl = cloudinaryService.uploadImage(imageFile);
+
+            dto.setImageProduct(imageUrl);
+        } else {
+
+            throw new RuntimeException("Image file is required for new product!");
+        }
+
+        processThumbnails(dto, thumbnailFiles);
+
         Product product = productMapper.toEntity(dto);
         product.setCategory(category);
+        product.setThumbnails(dto.getThumbnails());
+
+        calculateAndSetPrice(product);
 
         productRepository.save(product);
         return productMapper.toResponseDTO(product);
     }
 
-    //Update product
+    //UPDATE PRODUCT
     @Transactional
     @Override
-    public ProductResponseDTO updateProduct(Long id, ProductRequestDTO dto) {
+    public ProductResponseDTO updateProduct(Long id, ProductRequestDTO dto, MultipartFile imageFile, List<MultipartFile> thumbnailFiles) throws IOException {
         Product product = productRepository.findById(id)
                 .orElseThrow(() -> new NotFoundException("Product not found with ID: " + id));
 
-        // Update fields (if provided)
+        if (imageFile != null && !imageFile.isEmpty()) {
+            String imageUrl = cloudinaryService.uploadImage(imageFile);
+            product.setImageProduct(imageUrl);
+        } else if (dto.getImageProduct() != null) {
+            product.setImageProduct(dto.getImageProduct());
+        }
+
+        List<String> newUploadedUrls = new ArrayList<>();
+        if (thumbnailFiles != null && !thumbnailFiles.isEmpty()) {
+            for (MultipartFile file : thumbnailFiles) {
+                if (!file.isEmpty()) {
+                    String url = cloudinaryService.uploadImage(file);
+                    newUploadedUrls.add(url);
+                }
+            }
+        }
+
+        boolean hasUpdate = (dto.getThumbnails() != null) || (!newUploadedUrls.isEmpty());
+
+        if (hasUpdate) {
+            List<String> finalThumbnails = new ArrayList<>();
+
+            if (dto.getThumbnails() != null) {
+                finalThumbnails.addAll(dto.getThumbnails());
+            }
+
+            finalThumbnails.addAll(newUploadedUrls);
+
+            product.setThumbnails(finalThumbnails);
+        }
+
         if (dto.getNameProduct() != null) product.setNameProduct(dto.getNameProduct());
         if (dto.getBrand() != null) product.setBrand(dto.getBrand());
-        if (dto.getPrice() != null) product.setPrice(dto.getPrice());
         if (dto.getOldPrice() != null) product.setOldPrice(dto.getOldPrice());
         if (dto.getDiscountPercent() != null) product.setDiscountPercent(dto.getDiscountPercent());
+
+        if (dto.getPrice() != null) product.setPrice(dto.getPrice());
+
         if (dto.getStockQuantity() != null) product.setStockQuantity(dto.getStockQuantity());
-        if (dto.getImageProduct() != null) product.setImageProduct(dto.getImageProduct());
         if (dto.getDescription() != null) product.setDescription(dto.getDescription());
         if (dto.getInformation() != null) product.setInformation(dto.getInformation());
+
         if (dto.getProductGroup() != null)
             product.setProductGroup(Enum.valueOf(ProductGroup.class, dto.getProductGroup()));
 
@@ -124,6 +212,8 @@ public class ProductServiceImpl implements ProductService {
             product.setCategory(category);
         }
 
+        calculateAndSetPrice(product);
+
         Product updated = productRepository.save(product);
         return productMapper.toResponseDTO(updated);
     }
@@ -132,28 +222,60 @@ public class ProductServiceImpl implements ProductService {
     @Transactional
     @Override
     public void deleteProduct(Long id) {
+//        Product product = productRepository.findById(id)
+//                .orElseThrow(() -> new NotFoundException("Product not found with ID: " + id));
+//        productRepository.delete(product);
+
         Product product = productRepository.findById(id)
                 .orElseThrow(() -> new NotFoundException("Product not found with ID: " + id));
-        productRepository.delete(product);
+
+
+        product.setActive(false);
+
+        productRepository.save(product);
+
+        System.out.println("Đã xóa mềm sản phẩm ID: " + id);
     }
 
-    //filter product
     @Override
     public List<ProductResponseDTO> filterProducts(String brand, Double minPrice, Double maxPrice, Long categoryId, String sort) {
-        List<Product> products = productRepository.filterProducts(brand, minPrice, maxPrice, categoryId);
+
+        Sort sortOption = Sort.unsorted();
+
+        if (sort != null) {
+            switch (sort.toLowerCase()) {
+                case "price_low_to_high":
+                    sortOption = Sort.by(Sort.Direction.ASC, "price");
+                    break;
+                case "price_high_to_low":
+                    sortOption = Sort.by(Sort.Direction.DESC, "price");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        List<Product> products = productRepository.filterProducts(brand, minPrice, maxPrice, categoryId, sortOption);
 
         if (products.isEmpty()) {
+
             throw new NotFoundException("No products found with given filters");
         }
 
-        // Sort (Low to High / High to Low)
-        if (sort != null) {
-            switch (sort.toLowerCase()) {
-                case "price_low_to_high" -> products.sort((a, b) -> Double.compare(a.getPrice(), b.getPrice()));
-                case "price_high_to_low" -> products.sort((a, b) -> Double.compare(b.getPrice(), a.getPrice()));
-                default -> {} // "featured" or null: no sort
-            }
+        return products.stream()
+                .map(productMapper::toResponseDTO)
+                .collect(Collectors.toList());
+    }
+
+
+    @Override
+    public List<ProductResponseDTO> searchProducts(String keyword) {
+
+        if (keyword == null || keyword.trim().isEmpty()) {
+            return List.of();
         }
+
+        List<Product> products = productRepository.findByNameProductContainingIgnoreCase(keyword.trim());
 
         return products.stream()
                 .map(productMapper::toResponseDTO)

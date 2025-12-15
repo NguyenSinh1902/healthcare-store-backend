06696fe33cabc1325bd7bf48371c48c14a030860
NguyenSinh1902 <lenguyensinh1902@gmail.com>
2025-12-15 11:35:11 +0700

    feat(payment): add PaymentRepository

---
+++ src/main/java/iuh/fit/se/repositories/PaymentRepository.java
@@ -0,0 +1,17 @@
+package iuh.fit.se.repositories;
+
+import iuh.fit.se.entities.payment.Payment;
+import iuh.fit.se.entities.payment.PaymentStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    Optional<Payment> findByOrder_IdOrder(Long orderId);
+}

b538d7275ea1d5c001cf553aa0033419c5f19ffb
MihnThihn <pvmthinh2002@gmail.com>
2025-12-15 12:32:45 +0700

    feat(payment): add dtos & mapper(payment)

---
+++ src/main/java/iuh/fit/se/dtos/payment/PaymentRequestDTO.java
@@ -0,0 +1,18 @@
+package iuh.fit.se.dtos.payment;
+
+import jakarta.validation.constraints.NotNull;
+
+public class PaymentRequestDTO {
+    @NotNull(message = "Order ID is required")
+    private Long orderId;
+
+    private String bankCode;
+
+    public PaymentRequestDTO() {}
+    public PaymentRequestDTO(Long orderId) { this.orderId = orderId; }
+
+    public Long getOrderId() { return orderId; }
+    public void setOrderId(Long orderId) { this.orderId = orderId; }
+    public String getBankCode() { return bankCode; }
+    public void setBankCode(String bankCode) { this.bankCode = bankCode; }
+}
